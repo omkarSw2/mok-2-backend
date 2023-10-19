f95ae8f78fc2ff38bc6eba7c460560e653a52dd5
omkarSw2 <omkar21143@gmail.com>
2023-10-19 19:54:36 +0530

    final commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { conntection } = require("./db");
+const { EventModel } = require("./model/events.model");
 
 require("dotenv").config;
 const cors = require("cors");
@@ -7,10 +8,24 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 app.get("/", async (req, res) => {
   try {
-    return res.status(200).send({ msg: "Home Route" });
+    const data = await EventModel.find();
+    return res.status(200).send({ msg: "get data", events: data });
   } catch (error) {
+    console.log(error);
+    return res.status(400).send({ msg: error.message });
+  }
+});
+
+app.post("/add", async (req, res) => {
+  try {
+    const data = await EventModel(req.body);
+    await data.save();
+    return res.status(200).send({ msg: "get data", events: data });
+  } catch (error) {
+    console.log(error);
     return res.status(400).send({ msg: error.message });
   }
 });
---
+++ model/events.model.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const eventsSchema = mongoose.Schema({
+  poster: { type: String },
+  name: { type: String },
+  description: { type: String },
+  date: { type: String },
+  location: { type: String },
+  category: { type: String },
+  price: { type: Number },
+});
+
+const EventModel = mongoose.model("event", eventsSchema);
+module.exports = { EventModel };
